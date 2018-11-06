853d2ddd7c8403307c6e95dafde5b850cc422c6d
Tantely Randria <tantely.tnr@gmail.com>
2018-11-06 10:46:41 +0100

    atelier 05NOV18

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+class HelloWorld
+{
+
+		public static void main(String[] args)
+	{
+    	
+		int hours;
+		hours=0;
+		if (hours>=6 && hours<=20)
+		{
+		System.out.println("Hello World");
+		}
+		else
+		{
+		System.out.println("Goodnight world");
+		}
+	}
+
+}
+
